b0cf6577ef88d9e715d91d4d8b0b771170e88c87
Abdelrahman Hassan Soliman <abdelrahman.h.soliman@outlook.com>
2022-05-12 03:30:44 +0200

    with dictionary & kruskal(beta)

--- ImageQuantization/ImageOperations.cs
+++ ImageQuantization/ImageOperations.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
+using System.Linq;
 ///Algorithms Project
 ///Intelligent Scissors
 ///
@@ -93,6 +94,155 @@ namespace ImageQuantization
             return Buffer;
         }
 
+        //PRIORY Queue implementation
+        public class PriorityQueue<T>
+        {
+            class Node
+            {
+                public int Priority { get; set; }
+                public T Object { get; set; }
+            }
+
+            //object array
+            List<Node> queue = new List<Node>();
+            int heapSize = -1;
+            bool _isMinPriorityQueue;
+            public int Count { get { return queue.Count; } }
+
+            /// <summary>
+            /// If min queue or max queue
+            /// </summary>
+            /// <param name="isMinPriorityQueue"></param>
+            public PriorityQueue(bool isMinPriorityQueue = false)
+            {
+                _isMinPriorityQueue = isMinPriorityQueue;
+            }
+
+
+            public void Enqueue(int priority, T obj)
+            {
+                Node node = new Node() { Priority = priority, Object = obj };
+                queue.Add(node);
+                heapSize++;
+                //Maintaining heap
+                if (_isMinPriorityQueue)
+                    BuildHeapMin(heapSize);
+                else
+                    BuildHeapMax(heapSize);
+            }
+            public T Dequeue()
+            {
+                if (heapSize > -1)
+                {
+                    var returnVal = queue[0].Object;
+                    queue[0] = queue[heapSize];
+                    queue.RemoveAt(heapSize);
+                    heapSize--;
+                    //Maintaining lowest or highest at root based on min or max queue
+                    if (_isMinPriorityQueue)
+                        MinHeapify(0);
+                    else
+                        MaxHeapify(0);
+                    return returnVal;
+                }
+                else
+                    throw new Exception("Queue is empty");
+            }
+            public void UpdatePriority(T obj, int priority)
+            {
+                int i = 0;
+                for (; i <= heapSize; i++)
+                {
+                    Node node = queue[i];
+                    if (object.ReferenceEquals(node.Object, obj))
+                    {
+                        node.Priority = priority;
+                        if (_isMinPriorityQueue)
+                        {
+                            BuildHeapMin(i);
+                            MinHeapify(i);
+                        }
+                        else
+                        {
+                            BuildHeapMax(i);
+                            MaxHeapify(i);
+                        }
+                    }
+                }
+            }
+            private void BuildHeapMax(int i)
+            {
+                while (i >= 0 && queue[(i - 1) / 2].Priority < queue[i].Priority)
+                {
+                    Swap(i, (i - 1) / 2);
+                    i = (i - 1) / 2;
+                }
+            }
+            /// <summary>
+            /// Maintain min heap
+            /// </summary>
+            /// <param name="i"></param>
+            private void BuildHeapMin(int i)
+            {
+                while (i >= 0 && queue[(i - 1) / 2].Priority > queue[i].Priority)
+                {
+                    Swap(i, (i - 1) / 2);
+                    i = (i - 1) / 2;
+                }
+            }
+            private void MaxHeapify(int i)
+            {
+                int left = ChildL(i);
+                int right = ChildR(i);
+
+                int heighst = i;
+
+                if (left <= heapSize && queue[heighst].Priority < queue[left].Priority)
+                    heighst = left;
+                if (right <= heapSize && queue[heighst].Priority < queue[right].Priority)
+                    heighst = right;
+
+                if (heighst != i)
+                {
+                    Swap(heighst, i);
+                    MaxHeapify(heighst);
+                }
+            }
+            private void MinHeapify(int i)
+            {
+                int left = ChildL(i);
+                int right = ChildR(i);
+
+                int lowest = i;
+
+                if (left <= heapSize && queue[lowest].Priority > queue[left].Priority)
+                    lowest = left;
+                if (right <= heapSize && queue[lowest].Priority > queue[right].Priority)
+                    lowest = right;
+
+                if (lowest != i)
+                {
+                    Swap(lowest, i);
+                    MinHeapify(lowest);
+                }
+            }
+            private void Swap(int i, int j)
+            {
+                var temp = queue[i];
+                queue[i] = queue[j];
+                queue[j] = temp;
+            }
+            private int ChildL(int i)
+            {
+                return i * 2 + 1;
+            }
+            private int ChildR(int i)
+            {
+                return i * 2 + 2;
+            }
+        }
+        //========================End of priorty Queue=========================\\
+        
         public class Quantizer {
             public int k;
             public int DistinctColours;
@@ -101,11 +251,26 @@ namespace ImageQuantization
             private RGBPixel[,] quantizedImage;
             private HashSet<RGBPixel> DistinctColorsSet;
             public Dictionary<KeyValuePair<RGBPixel, RGBPixel>, float> distances;
+            public Dictionary<KeyValuePair<RGBPixel,RGBPixel>, float> MST;
+            public List<HashSet<RGBPixel>> vSets;
+            public int[,] adjMatrix;
+
+
+
+            public class vertex {
+                public int id;
+                public int parent = -1;
+                public int weight;
+                public bool isgray = false;
+            }
+
             public Quantizer()
             {
                 this.k = 1;
                 this.DistinctColorsSet = new HashSet<RGBPixel>();
                 this.distances = new Dictionary<KeyValuePair<RGBPixel, RGBPixel>, float>();
+                this.MST = new Dictionary<KeyValuePair<RGBPixel, RGBPixel>, float>();
+                this.vSets = new List<HashSet<RGBPixel>>();
             }
             public void quantize(RGBPixel[,] imageMatrix, int k)
             {
@@ -150,6 +315,34 @@ namespace ImageQuantization
 
                 float dist = (float)((p1.red + p2.red) + (p1.green + p2.green) + (p1.blue + p2.blue));
                 return (float) Math.Sqrt(dist);
+            }
+
+            public void generatMST()
+            {
+                int i = 0;
+                distances.OrderBy(Key => Key.Value);
+                HashSet<RGBPixel> currentSet;
+                HashSet<RGBPixel> nextSet;
+                List<RGBPixel> MStree = new List<RGBPixel>();
+
+                foreach (var v in this.DistinctColorsSet)
+                {
+                    vSets.Add(new HashSet<RGBPixel>());
+                    vSets[i].Add(v);
+                    i++;
+                }
+                foreach (var p in this.distances)
+                {
+                    currentSet = vSets.Find(c => c.Contains(p.Key.Key));
+                    nextSet = vSets.Find(c => c.Contains(p.Key.Value));
+                    if (!currentSet.Equals(nextSet))
+                    { 
+                        currentSet.UnionWith(nextSet);
+                        MStree.Add(p.Key.Key);
+                        MStree.Add(p.Key.Value);
+                    }
+
+                }
             }
 
             // returns image for displaying
